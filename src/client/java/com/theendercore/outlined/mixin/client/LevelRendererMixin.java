6c994ecc3ab90791aaa86eed4ed50dc84ec2a836
The Typhothanian <thetyphothanian@gmail.com>
2025-12-12 09:51:13 -0700

    fuck that bullshit

--- src/client/java/com/theendercore/outlined/mixin/client/LevelRendererMixin.java
+++ src/client/java/com/theendercore/outlined/mixin/client/LevelRendererMixin.java
@@ -1,21 +1,15 @@
 package com.theendercore.outlined.mixin.client;
 
-import com.google.gson.JsonSyntaxException;
-import com.mojang.blaze3d.pipeline.RenderTarget;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.theendercore.outlined.client.OutlinedClient;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.PostChain;
-import net.minecraft.resources.ResourceLocation;
-import org.slf4j.Logger;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.io.IOException;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
@@ -23,41 +17,18 @@ public class LevelRendererMixin {
     @Final
     private Minecraft minecraft;
 
-    @Shadow
-    @Final
-    private static Logger LOGGER;
-
-    @Inject(
-            method = "initOutline",
-            at = @At("TAIL")
+    @WrapOperation(
+            method = "renderLevel",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/Entity;getTeamColor()I"
+            )
     )
-    private void initOutline(CallbackInfo ci) {
-        PostChain effect = OutlinedClient.getENTITY_EFFECT();
-        RenderTarget target;
-
-        if (effect != null) {
-            effect.close();
+    private int renderLevel(Entity instance, Operation<Integer> original) {
+        if (minecraft.player != null && minecraft.player.hasLineOfSight(instance) && !instance.isCurrentlyGlowing()) {
+            return OutlinedClient.config.getColor().toInt();
         }
 
-        ResourceLocation location = OutlinedClient.INSTANCE.id("shaders/post/entity_outline.json");
-
-        try {
-            effect = new PostChain(
-                    minecraft.getTextureManager(), minecraft.getResourceManager(), minecraft.getMainRenderTarget(), location
-            );
-            effect.resize(minecraft.getWindow().getWidth(), minecraft.getWindow().getHeight());
-            target = effect.getTempTarget("final");
-        } catch (IOException var3) {
-            LOGGER.warn("Failed to load shader: {}", location, var3);
-            effect = null;
-            target = null;
-        } catch (JsonSyntaxException var4) {
-            LOGGER.warn("Failed to parse shader: {}", location, var4);
-            effect = null;
-            target = null;
-        }
-
-        OutlinedClient.setENTITY_EFFECT(effect);
-        OutlinedClient.setOUTLINE_TARGET(target);
+        return original.call(instance);
     }
 }
